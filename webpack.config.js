db0f09c03bb64a9e367a37f39a85cdbb6455b5d5
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2019-05-26 11:50:52 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const BUILD_DIR = `${__dirname}/build`;
+const APP_DIR = `${__dirname}/src`;
+
+const config = {
+    watch: true,
+    entry: `${APP_DIR}/renderer_process.js`,
+    output: {
+        path: BUILD_DIR,
+        publicPath: '',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include : APP_DIR,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                  loader: 'css-loader',
+                  options: {
+                    modules: true
+                  }
+                })
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                query: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Electron React',
+            template: `${__dirname}/templates/index.hbs`
+        }),
+        new CopyWebpackPlugin([
+            {from:'public',to:'.'} 
+        ]),
+        
+        new ExtractTextPlugin({
+            filename: 'bundle.css',
+            disable: false,
+            allChunks: true
+        })        
+    ],
+    resolve: {
+      extensions: ['.js', '.json', '.jsx']
+    }
+}
+module.exports = config;
